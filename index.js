2732ae5c1ad8f926f3118b43a272076e0d293a42
sw7105 <grypkt2@gmail.com>
2024-11-28 08:37:12 +0000

    Cm4_1.3.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const { type } = require("os");
 const app = express();
 
 // Cm4_1.1.
@@ -12,15 +13,41 @@ app.get("/math/circle/:r", (req, res) => {
 });
 
 // Cm4_1.2.
-app.get("/math/rectangle/:a/:b", (req, res) => {
-  let a = parseFloat(req.params.a);
-  let b = parseFloat(req.params.b);
-  let area = a * b;
-  let perimeter = 2 * (a + b);
+app.get("/math/rectangle/:width/:height", (req, res) => {
+  let width = parseFloat(req.params.width);
+  let height = parseFloat(req.params.height);
+  let area = width * height;
+  let perimeter = width * (a + height);
   res.json({ area: area, perimeter: perimeter });
 });
 
-//TODO3
+// Cm4_1.3.
+app.get("/math/power/:base/:exponent", (req, res) => {
+  const { root } = req.query;
+
+  try {
+    let json = {};
+    let base = parseFloat(req.params.base);
+    let exponent = parseFloat(req.params.exponent);
+
+    if (isNaN(base) || isNaN(exponent)) {
+      throw new TypeError(
+        "Invalid input. Both base and exponent must be numbers.",
+      );
+    }
+
+    json.result = Math.pow(base, exponent);
+    if (root === "true") {
+      if (base < 0) {
+        throw new Error("Invalid input. Base must be non-negative.");
+      }
+      json.root = Math.sqrt(base);
+    }
+    res.json(json);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
